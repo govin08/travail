b92ea7383ba528d9e69166eebdfb2a3d21cecbfd
govin08 <govindah08@gmail.com>
2024-07-01 06:32:32 +0900

    two criteria : plot and name

--- cela/count.py
+++ cela/count.py
@@ -1,6 +1,5 @@
-character_count = 0 # 전체 등장인물
-main_char_count = 0 # 주요 등장인물 : 서사에 직접 등장 & 이름 호명
 with open('colmena_characters.txt', 'r', encoding='utf-8') as file:
+    lines = []
     for line in file:
         # 빈 행은 제외
         line = line.strip()
@@ -13,10 +12,18 @@ with open('colmena_characters.txt', 'r', encoding='utf-8') as file:
         if line.startswith('에필로그'):
             continue
         # 행 수를 합산하여 총 등장인물수 계산
-        character_count += 1
-        # 소괄호로 시작하는 행의 등장인물은 주요 등장인물이 아님
-        if line.startswith('('):
-            continue
-        main_char_count += 1
-print(character_count)
-print(main_char_count)
+        lines.append(line)
+
+print(f"전체 등장인물 : {len(lines)}")
+char_index_plot = set()
+char_index_name = set()
+
+for line in lines:
+    if not line.startswith('('): # 서사x or 이름x
+        char_index_plot.add(lines.index(line))
+    if line.endswith('*')    : # 이름o
+        char_index_name.add(lines.index(line))
+print(f"이름이 호명되는 등장인물 : {len(char_index_name)}")
+print(f"서사에 등장하면서 이름이 호명되는 등장인물 : {len(char_index_plot)}")
+for index in char_index_plot - char_index_name:
+    print(index, lines[index])
