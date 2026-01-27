5252ad5f68550d412eb2493f42559029f6fb058c
govin08 <govindah08@gmail.com>
2026-01-28 08:05:12 +0900

    send_balloons.py added

---
+++ 2026/0127_soop_button/send_balloons.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+import keyboard
+# import pyperclip
+
+print('sending is now ready.')
+
+x1, y1 = 1317, 896
+x2, y2 = -517, 884
+button_positions = {1:(x1,y1), 2:(x2,y2)}
+
+def send_balloons(monitor_idx:int, num:str):
+    x_star, y_star = button_positions[monitor_idx]
+    pyautogui.click(x_star, y_star)
+    time.sleep(0.3)
+
+    pyautogui.press('backspace', presses=2, interval=0.2)
+    keyboard.write(num)
+    pyautogui.press('tab', presses=4, interval=0.1)
+    time.sleep(0.2)
+
+    keyboard.write("잘 들었습니다.")
+
+    pyautogui.press('tab', presses=3, interval=0.1)
+    pyautogui.press('enter')
+
+keyboard.add_hotkey('F5', lambda: send_balloons(1, "5"))
+keyboard.add_hotkey('F6', lambda: send_balloons(2, "5"))
+
+keyboard.wait()  # 프로그램이 계속 실행되도록 유지
