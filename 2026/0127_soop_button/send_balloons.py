172bbac8feee4225b1322452ad1ad277768cf377
govin08 <govindah08@gmail.com>
2026-01-31 23:01:22 +0900

    lm

--- 2026/0127_soop_button/send_balloons.py
+++ 2026/0127_soop_button/send_balloons.py
@@ -5,8 +5,8 @@ import keyboard
 
 print('sending is now ready.')
 
-x1, y1 = 1317, 896
-x2, y2 = -517, 884
+x1, y1 = -517, 884
+x2, y2 = 1317, 896
 button_positions = {1:(x1,y1), 2:(x2,y2)}
 
 def send_balloons(monitor_idx:int, num:str):
@@ -24,7 +24,7 @@ def send_balloons(monitor_idx:int, num:str):
     pyautogui.press('tab', presses=3, interval=0.1)
     pyautogui.press('enter')
 
-keyboard.add_hotkey('F7', lambda: send_balloons(1, "5"))
+keyboard.add_hotkey('F6', lambda: send_balloons(1, "5"))
 keyboard.add_hotkey('F8', lambda: send_balloons(2, "5"))
 
 keyboard.wait()  # 프로그램이 계속 실행되도록 유지
